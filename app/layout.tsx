23e1b72319d08ab313586c9afba55e994f675409
Naeem Hussain <naeemnh@outlook.com>
2026-02-22 21:09:28 +0400

    feat: integrate sidebar in layout and add dashboard page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
+import { SVGTemplates } from "@/components/molecules";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <SVGTemplates />
       </body>
     </html>
   );
